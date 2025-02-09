51a54520662193cbfabde703badd3069cfa8279b
KarthikeyanR <rkarthikeyan578@gmail.com>
2025-02-09 17:51:50 +0530

    Initial commit

--- Bank-Account-Management-System/src/main/java/com/example/Bank/Account/Management/System/controller/AccountController.java
+++ Bank-Account-Management-System/src/main/java/com/example/Bank/Account/Management/System/controller/AccountController.java
@@ -3,6 +3,7 @@ package com.example.Bank.Account.Management.System.controller;
 import com.example.Bank.Account.Management.System.model.Account;
 import com.example.Bank.Account.Management.System.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,13 +12,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/Account")
 public class AccountController {
     @Autowired
     private AccountService accountService;
-
     @PostMapping("/create")
     public Account createAccount(@RequestBody Account account) {
         return accountService.CreateAccount(account.getAccountnumber(), account.getAccountholder(), account.getPin(), account.getCurrentbalance());
