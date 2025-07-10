33a302d70a6caa2c600944588f41c88fa68e5dca
Karthikeyan R <168166270+imrkarthikeyan@users.noreply.github.com>
2025-07-10 19:18:03 +0530

    Updated AccountController.java

--- Bank Account Management System/Bank-Account-Management-System/src/main/java/com/example/Bank/Account/Management/System/controller/AccountController.java
+++ Bank Account Management System/Bank-Account-Management-System/src/main/java/com/example/Bank/Account/Management/System/controller/AccountController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/Account")
 public class AccountController {
     @Autowired
